bd79064e53fba04347dab8453559e38cec31c354
Neil Edwards <neil@plasticwater.com>
2012-03-13 22:22:13 +0000

    added own view controller and date on load

---
+++ Five a day/NewEntryTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewEntryTableViewController.h
+//  Five a day
+//
+//  Created by Neil Edwards on 13/03/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NewEntryTableViewController : UITableViewController
+
+@end
